2b1e4386b99c68312e0a94e250dcae57d7c99a46
DwngLee <156188368+DwngLee@users.noreply.github.com>
2024-01-15 21:58:18 +0700

    khoi tao file be

---
+++ Software/BE/BE_IOT/src/test/java/com/example/duongle/be_iot/BeIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duongle.be_iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
